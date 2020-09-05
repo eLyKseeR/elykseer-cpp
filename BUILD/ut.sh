9da39e423e53697f1b9d14e82f2df8de67db3c05
Alexander Diemand <codieplusplus@apax.net>
2020-09-05 23:18:47 +0200

    build required directories
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- BUILD/ut.sh
+++ BUILD/ut.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+D=$TMPDIR/restored
+test -d $D || mkdir $D
+D=$TMPDIR/meta
+test -d $D || mkdir $D
+
 # --log_level=all --run_test=utRestoreCtrl
 
 #./test/utelykseer-cpp --show_progress  $*
+
 ./test/utelykseer-cpp --no_color_output  $*
